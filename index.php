e40abd971c0592aa514f8ccb67b7fb287ca51879
ErickLima <ericklimacosta29@gmail.com>
2022-11-11 13:34:01 -0300

    index Atualizado

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php 
 
 require_once(dirname(__FILE__, 2) . '/innout/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/innout/src/models/User.php');
+require_once(dirname(__FILE__, 2) . '/innout/src/views/login.php');
 
-$user = new User(['name' => 'Erick', 'email' => 'erick@gmail.com']);
-//echo $user->getSelect();
-//echo User::getSelect(['id' => '1'], 'name', 'email');
-
-print_r(User::get(['name' => 'Chaves'], 'id, name, email'));
-echo '<br>';
-
-foreach(User::get([], 'name') as $user) {
-    echo $user->name;
-    echo '<br>';
-}
