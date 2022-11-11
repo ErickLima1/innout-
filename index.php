35d50a32dafdb333d4079bb3f7e2cd92680c8aa2
ErickLima <ericklimacosta29@gmail.com>
2022-11-11 15:23:55 -0300

    index atualizado

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php 
 
 require_once(dirname(__FILE__, 2) . '/innout/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/innout/src/views/login.php');
+//require_once(VIEW_PATH. '/login.php');
 
+require_once(MODEL_PATH . '/Login.php');
+
+$login = new Login([
+    'email' => 'admin@cod3r.com.br',
+    'password' => 'a'
+]);
+
+try {
+    $login->checkLogin();
+    echo 'Deu Certo!';
+} catch(Exception $e) {
+    echo 'Problema No Login :P';
+}
