d1bff46db6bbe446fce2bc1eda05188cc60ad491
ErickLima <ericklimacosta29@gmail.com>
2022-11-15 13:02:26 -0300

    config Atualizado

--- src/config/config.php
+++ src/config/config.php
@@ -6,10 +6,13 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 //Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__). '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__). '/../views'));
+define('TEMPLATE_PATH', realpath(dirname(__FILE__). '/../views/template'));
 define('CONTROLLER_PATH', realpath(dirname(__FILE__). '/../controllers'));
+define('EXECPTION_PATH', realpath(dirname(__FILE__). '/../execptions'));
 
 //Arquivos
 require_once(realpath(dirname(__FILE__). '/database.php'));
 require_once(realpath(dirname(__FILE__). '/loader.php'));
 require_once(realpath(MODEL_PATH . '/Model.php'));
+require_once(realpath(EXECPTION_PATH . '/AppExecption.php'));
 
