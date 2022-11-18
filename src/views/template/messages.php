a8798d57238de86af93de20de22f6cf1b6972021
ErickLima <ericklimacosta29@gmail.com>
2022-11-17 22:28:25 -0300

    template\messages

--- src/views/template/messages.php
+++ src/views/template/messages.php
@@ -1,16 +1,37 @@
 <?php
+$errors = [];
 
-    if($exception) {
-        $message = [
-            'type' => 'error',
-            'message' => $exception->getMessage()
-        ];
+
+if (isset($_SESSION['message'])) {
+    $message = $_SESSION['message'];
+    unset($_SESSION['message']);
+} elseif (isset($exception)) {
+    $message = [
+        'type' => 'error',
+        'message' => $exception->getMessage()
+    ];
+
+    if (get_class($exception) === 'ValidationExecption') {
+        $errors = $exception->getErrors();
     }
+}
 
+$alertType = '';
+
+if (isset($message['type']) && $message['type'] === 'error') {
+    $alertType = 'danger';
+} else {
+    $alertType = 'success';
+}
 ?>
 
 <?php if (isset($message) && $message) : ?>
-    <div class="my-3 alert alert-danger" role="alert">
+    <div role="alert" class="my-3 alert alert-<?= $alertType ?>">
         <?= $message['message'] ?>
     </div>
 <?php endif ?>
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
