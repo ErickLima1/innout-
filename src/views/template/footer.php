01618a9278bd8ca99fea0d1e0cf7b13411e76562
ErickLima <ericklimacosta29@gmail.com>
2022-12-17 18:36:26 -0300

    footer.php

---
+++ src/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <span>Desenvolvido Com</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>Por 10<span class="text-danger">0</span></span>
+</footer>
+</body>
+</html>
