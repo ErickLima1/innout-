8537c4c7738d40a9b3f200fe279373c1e4f67424
ErickLima <ericklimacosta29@gmail.com>
2022-12-21 20:11:55 -0300

    update day_records/views

--- src/views/day_records.php
+++ src/views/day_records.php
@@ -1,14 +1,16 @@
 <main class="content">
-    <div class="content-title mb-4">
-        <i class="icon icofont-check-alt mr-2"></i>
-        <div>
-            <h1>Registrar Ponto</h1>
-            <h2>Mantenha Seu Ponto Consistente!</h2>
-        </div>
-    </div>
+    <?php
+    renderTitle(
+        'Registrar Ponto',
+        'Mantenha Seu Ponto Consistente!',
+        'icofont-check-alt'
+
+    );
+        include(TEMPLATE_PATH . "/messages.php");
+     ?>
     <div class="card">
         <div class="card-header">
-            <h3>19 de Dezembro de 2022</h3>
+            <h3><?= $today ?></h3>
             <p class="mb-0">Os Batimentos Efetuados Hoje</p>
         </div>
         <div class="card-body">
