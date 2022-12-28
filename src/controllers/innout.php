d1bd0c7bccf4e626d0f92cd7d078923d07425e6d
ErickLima <ericklimacosta29@gmail.com>
2022-12-28 18:24:10 -0300

    controllers/innout.php update

--- src/controllers/innout.php
+++ src/controllers/innout.php
@@ -9,6 +9,10 @@ $records = WorkingHours::loadFromUserAndDate($user->id, date('Y-m-d'));
 
 try{
     $currentTime = strftime('%H:%M:%S', time());
+    if($_POST['forcedTime']) {
+        $currentTime = $_POST['forcedTime'];
+    }
+    
     $records->innout($currentTime);
     addSucessMsg('Ponto Inserido Com Sucesso!');
 }catch(AppException $e) {
