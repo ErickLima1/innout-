590c43790bbd29b5f1d957b535c1167ce174626f
ErickLima <ericklimacosta29@gmail.com>
2022-12-29 17:07:32 -0300

    template/left.php Update

--- src/views/template/left.php
+++ src/views/template/left.php
@@ -36,7 +36,7 @@
             <i class="icon icofont-hour-glass text-primary"></i>
             <div class="info">
                 <span class="main text-primary">
-                    04:23
+                   <?= $workedInterval ?>
                 </span>
                 <span class="label text-muted">Horas Trabalhadas</span>
             </div>
@@ -46,7 +46,7 @@
                 <i class="icon icofont-ui-alarm text-danger"></i>
                 <div class="info">
                     <span class="main text-primary">
-                        18:00
+                        <?= $exitTime ?>
                     </span>
                     <span class="label text-muted">Horas de Saida</span>
                 </div>
