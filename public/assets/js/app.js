c24462d11c6fe675e684e08b5bd9688bb583f103
ErickLima <ericklimacosta29@gmail.com>
2023-01-02 15:43:23 -0300

    Update app.js

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -6,3 +6,29 @@
         body.classList.toggle('hide-sidebar');
     }
 })()
+
+function addOneSecond(hours, minutes, seconds) {
+    const d = new Date();
+    d.setHours(parseInt(hours));
+    d.setMinutes(parseInt(minutes));
+    d.setSeconds(parseInt(seconds) + 1);
+
+    const h = `${d.getHours()}`.padStart(2,0);
+    const m = `${d.getMinutes()}`.padStart(2,0);
+    const s = `${d.getSeconds()}`.padStart(2,0);
+
+    return `${h}:${m}:${s}`;
+}
+
+function activateClock() {
+    const activateClock = document.querySelector('[active-clock]');
+    if(!activateClock)  return;
+
+    setInterval(function() {
+        // '16:45:52 => ['07', '27', '19'];        
+        const parts = activateClock.innerHTML.split(':');
+        activateClock.innerHTML = addOneSecond(...parts);
+    }, 1000);
+}
+
+activateClock();
