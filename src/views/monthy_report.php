046109503e97eafa42f9b0f2654741f107c7f3c7
ErickLima <ericklimacosta29@gmail.com>
2023-01-10 21:43:17 -0300

    Update monthy_report.php

--- src/views/monthy_report.php
+++ src/views/monthy_report.php
@@ -7,6 +7,32 @@
     );
  ?>
  <div>
+	<form class="mb-4" action="#" method="post">
+		<div class="input-group">
+			<?php if($user->is_admin): ?>
+			<select name="user" class="form-control mr-2" placeholder="Selecionar o Usuario...">
+				<option value="">Selecione o Usúario</option>
+				<?php
+					foreach($users as $user) {
+						$selected = $user->id === $selectUserId ? 'selected' : '';
+						echo utf8_encode("<option value='{$user->id}' {$selected}>{$user->name}</option>");
+					}
+				?>
+			</select>
+			<?php endif ?>
+			<select name="period" class="form-control" placeholder="Selecionar o Periodo...">
+				<?php
+					foreach($periods as $key => $month) {
+						$selected = $key === $selectedPeriod ? 'selected' : '';
+						echo utf8_encode("<option value='{$key}' {$selected}>{$month}</option>");
+					}
+				?>
+			</select>
+			<button class="btn btn-primary ml-2">
+				<i class="icofont-search"></i>
+			</button>
+		</div>
+	</form>
     <table class="table table-bordered table-striped table-hover">
 		<thead>
 			<th>Dia</th>
@@ -19,7 +45,7 @@
 		<tbody>
 			<?php foreach($report as $registry): ?>
 				<tr>
-					<td><?= formatDateWithLocale($registry->work_date, '%A, %d de %B de %Y') ?></td>
+					<td><?= utf8_encode(formatDateWithLocale($registry->work_date, '%A, %d de %B de %Y')) ?></td>
 					<td><?=$registry->time1 ?></td>
 					<td><?=$registry->time2 ?></td>
 					<td><?=$registry->time3 ?></td>
@@ -31,7 +57,7 @@
 				<td>Horas Trabalhadas</td>
 				<td colspan="3"><?= $sumOfWorkedTime?></td>
 				<td>Saldo Mensal</td>
-				<td><?= $balance?></td>
+				<td><?= $balance ?></td>
 			</tr>
 		</tbody>
     </table>
