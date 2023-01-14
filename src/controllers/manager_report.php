3948ebf32a45d6365b40ee32607065216ab8df65
ErickLima <ericklimacosta29@gmail.com>
2023-01-14 17:18:00 -0300

    Update manager.report/controllers

--- src/controllers/manager_report.php
+++ src/controllers/manager_report.php
@@ -2,4 +2,17 @@
 session_start();
 RequireValidSession();
 
-loadTemplateView('manager_report', []);
+//Tudo isso vai para Interface Grafica do Monthy_Report viesw
+$activeUsersCount = User::getActiveUsersCount();
+$absentUsers = WorkingHours::getAbsentUsers();
+
+$yearAndMonth = (new DateTime())->format('Y-m');
+$seconds = WorkingHours::getWorkedTimeInMoth($yearAndMonth);
+//Funcionalidade do Explode: ele define uma caractere ele transforma uma String em um Array
+$hoursInMonth = explode(':', getTimeStringFromSeconds($seconds))[0]; 
+
+loadTemplateView('manager_report', [
+    'activeUsersCount' => $activeUsersCount,
+    'absentUsers' => $absentUsers,
+    'hoursInMonth' => $hoursInMonth,
+]);
