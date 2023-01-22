2d0d97058fa1132079af1ec159c24888c4569505
ErickLima <ericklimacosta29@gmail.com>
2023-01-22 14:04:25 -0300

    views/users

---
+++ src/controllers/save_user.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+RequireValidSession();
+
+
+//Tratando erro caso na hora de inserir de error
+$exception = null;
+$userData = [];
+
+if (count($_POST) === 0 && isset($_GET['update'])) {
+    $user = User::getOne(['id' => $_GET['update']]);
+    $userData = $user->getValues();
+    $userData['password'] = null;
+} elseif (count($_POST) > 0) {
+    try {
+        $dbUser = new User($_POST);
+        if ($dbUser->id) {
+            $dbUser->update();
+            addSucessMsg('Usuário alterado com sucesso!');
+            header('Location: users.php');
+            exit();
+        } else {
+            $dbUser->id = null;
+            $dbUser->insert();
+            addSucessMsg('Usuário cadastrado com sucesso!');
+        }
+        $_POST = [];
+    } catch (Exception $e) {
+        $exception = $e;
+    } finally {
+        $userData = $_POST;
+    }
+}
+
+loadTemplateView('save_user', $userData + ['exception' => $exception]);
