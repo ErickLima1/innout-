c3d1316dc7dc05a7dc047ce2ad388e7da3b5a43c
ErickLima <ericklimacosta29@gmail.com>
2023-01-22 14:05:30 -0300

    models/user update

--- src/models/User.php
+++ src/models/User.php
@@ -16,4 +16,12 @@ class User extends model {
     public static function getActiveUsersCount() {
         return static::getCount(['raw' => 'end_date IS NULL']);
     }
+
+    //Meotod para inserir usuario;
+    public function insert() {
+        $this->is_admin = $this->is_admin ? 1 : 0; //Se tiver setado inteiro 1 caso contrario vai ser 0
+        if(!$this->end_date)  $this->end_date = null;
+        $this->password = password_hash($this->password, PASSWORD_DEFAULT);
+        return parent::insert();
+    }
 }
