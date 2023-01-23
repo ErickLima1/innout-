d2854706ffacff92e044eb094d254f32ebf4c0c0
ErickLima <ericklimacosta29@gmail.com>
2023-01-23 15:01:01 -0300

    Update save_user.php

--- src/views/save_user.php
+++ src/views/save_user.php
@@ -14,39 +14,39 @@
       <div class="form-row">
         <div class="form-group col-md-6">
             <label for="name">Nome</label>
-            <input type="text" id="name" placeholder="informe o nome" class="form-control
+            <input type="text" id="name" name="name" placeholder="informe o nome" class="form-control
                 <?= $errors['name'] ? 'is-invalid' : '' ?>" value="<?= $name ?? ""?>">
             <div class="invalid-feedback">
-                <? $errors['name'] ?>
+                <?= $errors['name'] ?>
             </div>
         </div>
         <div class="form-group col-md-6">
             <label for="email">E-mail</label>
             <input type="email" id="email" name="email" placeholder="Informe o email" class="form-control 
                 <?= $errors['email'] ? 'is-invalid' : '' ?>" value="<?= $email ?? "" ?>">
-             <div class="invalid-feedback">
+            <div class="invalid-feedback">
                 <?= $errors['email'] ?>
-                </div>
             </div>
+        </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
             <label for="password">Senha</label> <!--Parte da senha-->
-            <input type="password" id="password" placeholder="informe a senha" class="form-control
+            <input type="password" id="password" name="password" placeholder="informe a senha" class="form-control
                 <?= $errors['password'] ? 'is-invalid' : '' ?>">
             <div class="invalid-feedback">
-                <? $errors['password'] ?>
+                <?= $errors['password'] ?>
             </div>
         </div>
         <div class="form-group col-md-6"> <!--Parte da Senha-->
             <label for="confirm_password">Digite Novamente sua senha</label>
-            <input type="password" id="password" name="confirm_password" 
+            <input type="password" id="confirm_password" name="confirm_password" 
                 placeholder="Confirme a sua senha" class="form-control 
                 <?= $errors['confirm_password'] ? 'is-invalid' : '' ?>">
-             <div class="invalid-feedback">
+            <div class="invalid-feedback">
                 <?= $errors['confirm_password'] ?>
-                </div>
             </div>
+        </div>
       </div>
       <div class="form-row">
         <div class="form-group col-md-6">
@@ -54,11 +54,11 @@
             <input type="date" id="start_date" name="start_date" class="form-control
                 <?= $errors['start_date'] ? 'is-invalid' : '' ?>" value="<?= $start_date ?>">
             <div class="invalid-feedback">
-                <? $errors['start_date'] ?>
+                <?= $errors['start_date'] ?>
             </div>
         </div>
         <div class="form-group col-md-6">
-            <label for="confirm_password">Data de Desligamento</label>
+            <label for="end_date">Data de Desligamento</label>
             <input type="date" id="end_date" name="end_date" class="form-control 
                 <?= $errors['end_date'] ? 'is-invalid' : '' ?>" value="<?= $end_date ?>">
             <div class="invalid-feedback">
@@ -69,16 +69,17 @@
       <div class="form-row">
         <div class="form-group col-md-6">
             <label for="is_admin">Administrador</label>
-            <input type="checkbox" id="is_admin" class="form-control <?= $errors['is_admin'] ? 'is-invalid' : ''?>"
+            <input type="checkbox" id="is_admin" name="is_admin" 
+                class="form-control <?= $errors['is_admin'] ? 'is-invalid' : ''?>"
                 <?= isset($is_admin) && $is_admin ? 'checked' : '' ?>>
             <div class="invalid-feedback">
-                <? $errors['is_admin'] ?>
+                <?= $errors['is_admin'] ?>
             </div>
         </div>
       </div>
-      <div>
-        <button class="btn btn-primary btn-lg">Salvar</button>
-        <a href="/users.php" class="btn btn-secondary btn-lg">Cancelar</a>
-      </div>
+        <div>
+            <button class="btn btn-primary btn-lg">Salvar</button>
+            <a href="/users.php" class="btn btn-secondary btn-lg">Cancelar</a>
+        </div>
     </form>
 </main>
