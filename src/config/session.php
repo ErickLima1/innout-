b48215ffe2551b4c19729843bc77e49e5f05d83f
ErickLima <ericklimacosta29@gmail.com>
2023-01-28 16:06:30 -0300

    config/session Adicionando acesso negado caso o usuario não seja admin
    Adicionando uma forma para o usuario que não seja admin, não tenha acesso no usuarios nem no relatorio gerencial

--- src/config/session.php
+++ src/config/session.php
@@ -1,9 +1,14 @@
 <?php
 
-function RequireValidSession() {
+function RequireValidSession($requeresAdmin = false) {
     $user = $_SESSION['user'];
     if(!isset($user)) {
         header('Location: login.php');
         exit();
+    }elseif($requeresAdmin && !$user->is_admin) {
+        addErrorMsg('Acesso Negado!');
+        header('Location: day_records.php');
+        exit();
+
     }
 }
