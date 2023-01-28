dfc636043fe5f95b2f6b08a303fe3d35ef0e9986
ErickLima <ericklimacosta29@gmail.com>
2023-01-28 16:04:21 -0300

    Update footer.php

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <span>Desenvolvido Com</span>
     <span><i class="icofont-heart text-danger mx-1"></i></span>
-    <span>Por 10<span class="text-danger">0</span></span>
+    <span>Por Erick<span class="text-danger"></span></span>
 </footer>
 <script src="assets/js/app.js"></script>
 </body>
